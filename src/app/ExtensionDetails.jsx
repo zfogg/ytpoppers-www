756fd5e4bdd1ec13d1ba4f892737ca7ceecab969
Siddhartha Maity <siddharthamaity1993@gmail.com>
2024-10-28 16:43:13 +0530

    chore: Recommended extensions added

---
+++ src/app/ExtensionDetails.jsx
@@ -0,0 +1,71 @@
+const RECOMMENDED_EXTENSIONS = [
+    'formulahendry.auto-close-tag',
+    'aaron-bond.better-comments',
+    'mikestead.dotenv',
+    'EditorConfig.EditorConfig',
+    'dbaeumer.vscode-eslint',
+    'MikeBovenlander.formate',
+    'donjayamanne.githistory',
+    'wix.vscode-import-cost',
+    'sburg.vscode-javascript-booster',
+    'christian-kohler.npm-intellisense',
+    'esbenp.prettier-vscode',
+    'bradlc.vscode-tailwindcss',
+    'Gruntfuggly.todo-tree',
+    'ChakrounAnas.turbo-console-log',
+    'codeandstuff.package-json-upgrade',
+    'moalamri.inline-fold',
+    'KnisterPeter.vscode-commitizen',
+    'yzhang.markdown-all-in-one'
+];
+
+async function fetchExtensionDetails(extension) {
+    const response = await fetch('https://marketplace.visualstudio.com/_apis/public/gallery/extensionquery', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json;api-version=3.0-preview.1'
+        },
+        body: JSON.stringify({
+            filters: [
+                {
+                    criteria: [{ filterType: 7, value: extension }]
+                }
+            ],
+            flags: 914
+        })
+    });
+
+    const data = await response.json();
+    const extensionData = data.results[0].extensions[0];
+
+    // Extract relevant details
+    const downloadCount = extensionData.statistics.find((stat) => stat.statisticName === 'install').value;
+    const iconUri = extensionData.versions[0].files.find(
+        (file) => file.assetType === 'Microsoft.VisualStudio.Services.Icons.Default'
+    ).source;
+
+    return { name: extension, displayName: extensionData.displayName, downloadCount, iconUri };
+}
+
+const ExtensionDetails = async () => {
+    const extensionDetails = await Promise.all(RECOMMENDED_EXTENSIONS.map(fetchExtensionDetails));
+    console.log(extensionDetails);
+    return (
+        <div className='grid grid-cols-9 gap-6'>
+            {extensionDetails.map((extension) => {
+                return (
+                    <div key={extension.name} className='group relative inline-block'>
+                        <img className='size-9 hover:cursor-pointer' src={extension.iconUri} alt={extension.name} />
+                        <div className='absolute bottom-full left-1/2 mb-2 hidden -translate-x-1/2 transform space-y-1.5 whitespace-nowrap rounded bg-gray-800 px-3 py-3 text-sm text-white group-hover:block'>
+                            <h3 className='text-lg'>{extension.displayName}</h3>
+                            <p>Downloads: {extension.downloadCount.toLocaleString()}</p>
+                        </div>
+                    </div>
+                );
+            })}
+        </div>
+    );
+};
+
+export default ExtensionDetails;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import Image from 'next/image';
 
+import ExtensionDetails from '@/app/ExtensionDetails';
+
 export default function Home() {
     return (
         <div className='grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20'>
@@ -25,7 +27,7 @@ export default function Home() {
 
                 <div className='flex flex-col items-center gap-4 sm:flex-row'>
                     <a
-                        className='bg-foreground text-background flex h-10 items-center justify-center gap-2 rounded-full border border-solid border-transparent px-4 text-sm transition-colors hover:bg-[#383838] sm:h-12 sm:px-5 sm:text-base dark:hover:bg-[#ccc]'
+                        className='bg-foreground text-background flex h-10 items-center justify-center gap-2 rounded-full border border-solid border-transparent px-4 text-sm transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] sm:h-12 sm:px-5 sm:text-base'
                         href='https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app'
                         target='_blank'
                         rel='noopener noreferrer'>
@@ -39,7 +41,7 @@ export default function Home() {
                         Deploy now
                     </a>
                     <a
-                        className='flex h-10 items-center justify-center rounded-full border border-solid border-black/[.08] px-4 text-sm transition-colors hover:border-transparent hover:bg-[#f2f2f2] sm:h-12 sm:min-w-44 sm:px-5 sm:text-base dark:border-white/[.145] dark:hover:bg-[#1a1a1a]'
+                        className='flex h-10 items-center justify-center rounded-full border border-solid border-black/[.08] px-4 text-sm transition-colors hover:border-transparent hover:bg-[#f2f2f2] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] sm:h-12 sm:min-w-44 sm:px-5 sm:text-base'
                         href='https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app'
                         target='_blank'
                         rel='noopener noreferrer'>
@@ -47,6 +49,10 @@ export default function Home() {
                     </a>
                 </div>
             </main>
+            <div className='space-y-6 pt-9'>
+                <h2 className='text-center text-lg'>Extensions</h2>
+                <ExtensionDetails />
+            </div>
             <footer className='row-start-3 flex flex-wrap items-center justify-center gap-6'>
                 <a
                     className='flex items-center gap-2 hover:underline hover:underline-offset-4'
