674b98eeb85171464b40c3586121666ec52c295b
Siddhartha Maity <siddharthamaity1993@gmail.com>
2024-10-29 18:53:13 +0530

    refactor: Remove Tailwind CSS styling from HomePage component

--- src/app/(delete-this-and-modify-page.tsx)/HomePage.tsx
+++ src/app/(delete-this-and-modify-page.tsx)/HomePage.tsx
@@ -4,14 +4,14 @@ import ExtensionDetails from '@/app/(delete-this-and-modify-page.tsx)/ExtensionD
 import NavigationBar from '@/app/(delete-this-and-modify-page.tsx)/NavigationBar';
 import SetupDetails from '@/app/(delete-this-and-modify-page.tsx)/SetupDetails';
 
-import styles from './HomePage.module.css';
+import './HomePage.css';
 
 export default function Home() {
     return (
-        <div className={styles.page}>
+        <div className='page'>
             <NavigationBar />
-            <div className={styles.main}>
-                <Image className={styles.logo} src='/next.svg' alt='Next.js logo' width={180} height={38} priority />
+            <div className='main'>
+                <Image className='logo' src='/next.svg' alt='Next.js logo' width={180} height={38} priority />
                 <ol>
                     <li>
                         Get started by editing <code>src/app/page.tsx</code>.
@@ -19,27 +19,27 @@ export default function Home() {
                     <li>Save and see your changes instantly.</li>
                 </ol>
 
-                <div className={styles.ctas}>
+                <div className='ctas'>
                     <a
-                        className={styles.primary}
+                        className='primary'
                         href='https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
                         target='_blank'
                         rel='noopener noreferrer'>
-                        <Image className={styles.logo} src='/vercel.svg' alt='Vercel logomark' width={20} height={20} />
+                        <Image className='logo' src='/vercel.svg' alt='Vercel logomark' width={20} height={20} />
                         Deploy now
                     </a>
                     <a
                         href='https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
                         target='_blank'
                         rel='noopener noreferrer'
-                        className={styles.secondary}>
+                        className='secondary'>
                         Read our docs
                     </a>
                 </div>
                 <SetupDetails />
                 <ExtensionDetails />
             </div>
-            <footer className={styles.footer}>
+            <footer className='footer'>
                 <a
                     href='https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
                     target='_blank'
