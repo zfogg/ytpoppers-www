42b32036b7fe605dc99badb43ca59be878e90958
Zachary Fogg <zach.fogg@gmail.com>
2024-12-02 17:50:27 -0500

    kinda fixed the layout

--- src/app/components/ThemeSwitchLayout.tsx
+++ src/app/components/ThemeSwitchLayout.tsx
@@ -14,14 +14,15 @@ interface ThemeSwitchLayoutProps {
 const ThemeSwitchLayout: React.FC<ThemeSwitchLayoutProps> = ({ children }) => {
     return (
         <div className='flex min-h-screen flex-col'>
+            <div className='mt-8 flex self-end pr-[32px]'>
+                <ThemeSwitch />
+            </div>
             <div className='grid grid-cols-3'>
                 <div></div>
                 <Link className='mt-[80px] pb-6 sm:mt-[100px]' href='/'>
                     <NavigationBar />
                 </Link>
-                <div className='mt-8 flex pr-6'>
-                    <ThemeSwitch />
-                </div>
+                <div></div>
             </div>
             <div className='flex flex-1 items-center justify-center'>{children}</div>
         </div>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
 import type { ReactNode } from 'react';
 
-import type { Metadata } from 'next';
+import type { Metadata, Viewport } from 'next';
 import localFont from 'next/font/local';
-import Head from 'next/head';
 
 import { ThemeProvider } from 'next-themes';
 
@@ -24,17 +23,22 @@ export const metadata: Metadata = {
     description: 'Bringing YouTube "most popular videos" listings back to you.'
 };
 
+// INFO: https://nextjs.org/docs/app/api-reference/functions/generate-viewport#width-initialscale-maximumscale-and-userscalable
+export const viewport: Viewport = {
+    width: 'device-width',
+    initialScale: 1,
+    maximumScale: 1,
+    userScalable: false,
+    // Also supported by less commonly used
+    // interactiveWidget: 'resizes-visual',
+    colorScheme: 'dark'
+};
+
 const Layout = ({ children }: Readonly<{ children: ReactNode }>) => {
     return (
         // ? https://github.com/pacocoursey/next-themes?tab=readme-ov-file#with-app
         // ? https://react.dev/reference/react-dom/client/hydrateRoot#suppressing-unavoidable-hydration-mismatch-errors
         <html suppressHydrationWarning lang='en'>
-            <head>
-                <meta
-                    name='viewport'
-                    content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0'
-                />
-            </head>
             <body className={`${geistSans.variable} ${geistMono.variable} bg-background text-foreground antialiased`}>
                 <ThemeProvider attribute='class'>{children}</ThemeProvider>
             </body>
