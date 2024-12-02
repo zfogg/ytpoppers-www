f2d7d21eeb5a005d86401a766f31caec276322f3
Zachary Fogg <zach.fogg@gmail.com>
2024-12-02 15:03:23 -0500

    turnoff standalone output

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,7 @@ const withBundleAnalyzer = initializeBundleAnalyzer({
 
 // https://nextjs.org/docs/pages/api-reference/next-config-js
 const nextConfig: NextConfig = {
-    output: 'standalone'
+    //output: 'standalone'
 };
 
 export default withBundleAnalyzer(nextConfig);
